3a6f13ba04524a46c5ef7af4a97fc9ced07a648b
islizeqiang <islizeqiang@gmail.com>
2021-04-22 14:43:09 +0800

    feat: add koa-router example

--- example/koa-example/index.js
+++ example/koa-example/index.js
@@ -1,5 +1,25 @@
 import { PORT } from './config';
 
-fetch(`http://127.0.0.1:${PORT}`)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const callback = (response) => response.json().then((data) => console.log(data));
+
+const base = `http://127.0.0.1:${PORT}`;
+console.log(1);
+Promise.all([
+  fetch(`${base}`).then(callback),
+
+  fetch(`${base}/api/get`).then(callback),
+
+  fetch(`${base}/api/users`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user: {
+        name: 'John',
+        email: 'john@example.com',
+      },
+    }),
+  }).then(callback),
+]);
--- example/koa-example/server.js
+++ example/koa-example/server.js
@@ -1,24 +1,52 @@
 import Koa from '../../mini-lib/Koa';
+import Router from '../../mini-lib/KoaRouter';
 import { PORT } from './config';
 
 const app = new Koa();
+const router = new Router();
+
+app.use(async (ctx, next) => {
+  ctx.response.setHeader('Access-Control-Allow-Origin', '*');
+  ctx.response.setHeader('Access-Control-Max-Age', '10');
+  ctx.response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  await next();
+});
 
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
-  console.log(`${ctx.req.method} ${ctx.req.url} - ${ms}ms`);
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${ms}ms`);
 });
 
-app.use((ctx, next) => {
-  ctx.res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
-
-app.use((ctx, next) => {
+router.get('/', (ctx, next) => {
+  ctx.response.statusCode = 200;
   ctx.body = JSON.stringify('Hello World');
-  // next();
 });
+
+router.get('/api/get', async (ctx) => {
+  ctx.response.statusCode = 200;
+  ctx.body = {
+    id: 1,
+    name: '小明',
+    age: 18,
+  };
+});
+
+router.post('/api/users', async (ctx, next) => {
+  const body = await new Promise((resolve, reject) => {
+    const chunks = [];
+    ctx.request.on('data', (chunk) => chunks.push(chunk));
+    ctx.request.on('end', () => {
+      resolve(Buffer.concat(chunks));
+    });
+    ctx.request.on('error', (error) => reject(error));
+  });
+  ctx.response.statusCode = 200;
+  ctx.body = body;
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}/`);
